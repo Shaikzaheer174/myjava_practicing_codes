febd0bb54a502ddb5cb49f6bdfffd1ae36944df3
shaikzaheer174 <shaikzaheer174@gmail.com>
2022-08-25 00:07:58 +0530

    second commit

--- src/basics/hello_world.java
+++ src/basics/hello_world.java
@@ -1,7 +1,10 @@
 package basics;
 
-public class hello_world {
-	public static void main(String[] args) {
+public class hello_world 
+{
+
+	public static void main(String[] args) 
+	{
 		System.out.println("Hello world");
 	}
 
--- src/basics/reading_input_from_keyboard.java
+++ src/basics/reading_input_from_keyboard.java
@@ -8,24 +8,28 @@ public class reading_input_from_keyboard {
 
 		Scanner read = new Scanner(System.in);
 
-		int FirstNumber;
-
+		/*int FirstNumber;
 		int SecondNumber;
-
 		int Result;
 
 		System.out.println("enter first number");
-
 		FirstNumber = read.nextInt();
-
+		
 		System.out.println("enter second number");
-
 		SecondNumber = read.nextInt();
-
+		
 		Result = FirstNumber + SecondNumber;
-
+		
 		System.out.println("the result is " + Result);
-
+		*/
+		
+		
+//		useRadix to get the particular number  of binary number
+		read.useRadix(2); // 2 means binary number
+		int x = read.nextInt();
+		System.out.println(x);
+		
+		
 	}
 
 }
